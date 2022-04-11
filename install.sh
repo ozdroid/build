5b0710f7f28e17e08b150fa41eedfae9219231fb
ozdroid <ozgamel@gmail.com>
2022-04-12 05:56:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ make -j8 download
 make -j$(($(nproc) + 1)) V=s
 time=$(date "+%Y%m%d-%H%M%S")
 echo "------------------------------------------------------------- Uploading File -------------------------------------------------------------------------"
-sshpass -p "......" scp -r ./bin taco@192.168.2.249:/mnt/usb/lede/${time}
+sshpass -p "password" scp -r ./bin taco@192.168.2.249:/mnt/usb/lede/${time}
